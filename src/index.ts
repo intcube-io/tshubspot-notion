f7149f3688b62ae003a7167b8cc6740ddeabec36
Janis König <janis.koenig@intcube.io>
2023-10-17 11:58:10 +0000

    Notion: move DB id into .env file

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ async function main() {
   });
 
   const response = await notion.databases.query({
-    database_id: "FIXME",
+    database_id: process.env.NOTION_INTCUBE_PROJECT_DB!,
   });
 
   console.log("Got response:", response);
